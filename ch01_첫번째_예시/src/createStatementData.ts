87777f9ba2bfd4fa724b10947333753f9aa76ba6
SeolDonghun <ssalssi1@gmail.com>
2023-04-22 09:54:10 +0900

    separate subclasses with duck-typing and use factory method to create calculator instance

--- ch01_첫번째_예시/src/createStatementData.ts
+++ ch01_첫번째_예시/src/createStatementData.ts
@@ -7,12 +7,60 @@ import {
   EnrichedPerformance,
 } from './types';
 
+function createPerformanceCalculator(aPerformance: Performance, aPlay: Play) {
+  switch (aPlay.type) {
+    case 'tragedy':
+      return new TragedyCalculator(aPerformance, aPlay);
+    case 'comedy':
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장로: ${aPlay.type}`);
+  }
+}
+
 class performanceCalculator {
   performance: Performance;
   play: Play;
   constructor(aPerformance: Performance, aPlay: Play) {
     this.performance = aPerformance;
     this.play = aPlay;
+  }
+
+  get amount(): number {
+    throw new Error('서브클래스에서 처리하도록 설계되었습니다.');
+  }
+
+  get volumeCredits(): number {
+    return Math.max(this.performance.audience - 30, 0);
+  }
+}
+
+class TragedyCalculator extends performanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+  get volumeCredits(): number {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+    return result;
+  }
+}
+
+class ComedyCalculator extends performanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+  get volumeCredits(): number {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
 
@@ -25,50 +73,19 @@ export default function createStatementData(invoice: Invoice, plays: Plays) {
   return result;
 
   function enrichPerformance(aPerformance: Performance) {
-    const calculator = new performanceCalculator(
+    const calculator = createPerformanceCalculator(
       aPerformance,
       playFor(aPerformance)
     );
     const result = Object.assign({}, aPerformance) as EnrichedPerformance;
     result.play = calculator.play;
-    result.amount = amountFor(result);
-    result.volumsCredits = volumeCreditsFor(result);
+    result.amount = calculator.amount;
+    result.volumsCredits = calculator.volumeCredits;
     return result;
   }
 
   function playFor(aPerformance: Performance): Play {
     return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance: EnrichedPerformance): number {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-    }
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance: EnrichedPerformance): number {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ('comedy' == aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
   }
 
   function totalAmount(data: StatementData) {
